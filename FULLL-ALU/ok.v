9b1d58542ff80de32087261ee90d132cd42b3ede
IrvingCeledon <irvingdarey@gmail.com>
2025-09-06 13:11:11 -0600

    Update ok.v

--- FULLL-ALU/ok.v
+++ FULLL-ALU/ok.v
@@ -1,1 +1,16 @@
+module Pre_ALU(input[3:0] A, input[3:0] B, input Sel[1:0], output reg[3:0] C);
 
+// 2.- Internal components -> N/A
+
+// 3.- Assignments, Sequential Blocks, and Module Instances:
+
+always @*
+begin 
+ case (Sel) // Doesn't use begin
+   1'b0 : C = A + B;
+   1'b1 : C = A & B;
+   default : C = 4'b0000; // Best Practices?
+ endcase
+end
+  
+endmodule 
