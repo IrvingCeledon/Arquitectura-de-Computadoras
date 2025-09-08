323145217fe2805c8212d58c0bbd29896f7d818d
IrvingCeledon <irvingdarey@gmail.com>
2025-09-08 17:03:24 -0600

    Update _LOGIC-GATES.v

--- Actividad-2_/_LOGIC-GATES.v
+++ Actividad-2_/_LOGIC-GATES.v
@@ -1,1 +1,24 @@
+module _LOGIC_GATES(
+  input A,
+  input B,
+  output _AND,
+  output _NAND,
+  output _OR,
+  output _NOR,
+  output _NOT, 
+  output _XOR,
+  output _XNOR);
 
+// 2.- Internal components -> N/A
+
+// 3.- Assignments, Sequential Blocks, and Module Instances:
+
+assign _AND = A&B; 
+assign _NAND = ~(A&B); 
+assign _OR = A|B; 
+assign _NOR = ~(A|B); 
+assign _NOT = ~A; 
+assign _XOR = A^B; 
+assign _XNOR = ~(A^B); 
+
+endmodule
