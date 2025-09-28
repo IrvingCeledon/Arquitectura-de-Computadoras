bc1b4a2a9c6e01e4ceadbb41598a88720cf42295
IrvingCeledon <irvingdarey@gmail.com>
2025-09-27 23:15:23 -0600

    Update ram_async_tb.v

--- Tipos memoria/RAM-ASYNC/ram_async_tb.v
+++ Tipos memoria/RAM-ASYNC/ram_async_tb.v
@@ -1,45 +1,31 @@
 `timescale 1ns/1ns
 
 module _ram_tb();
+	reg [4:0] address_tb;
+	reg writeOn_tb;
+	reg [31:0] data_in_tb;
+	wire [31:0] data_out;
 
-reg [4:0] address_tb, RR2_tb, WriteReg_tb;
-reg [31:0] WriteData_tb;
-reg RegWrite_tb;
-wire [31:0] RD1_tb, RD2_tb;
+ram DUV (.address(address_tb), 
+		 .writeOn(writeOn_tb), 
+		 .data_in(data_in_tb), 
+		 .data_out(data_out));
 
-br DUV (.RR1(RR1_tb), .RR2(RR2_tb), .WriteReg(WriteReg_tb), 
-	.WriteData(WriteData_tb), 
-	.RegWrite(RegWrite_tb),
-	.RD1(RD1_tb), .RD2(RD2_tb));
+	reg [38:0] instructions [0:4]; 
 
 initial
 
 begin
-$readmemb("Data.txt", DUV.BR);
-  
-//1. Module definition
-module ram (address, data_in, data_out, writeOn);
-  input [5:0] address;
-  input [31:0] data_in;
-  output reg [31:0] data_out;
-  input writeOn;
+	$readmemb("instructions.txt", instructions);
+
+	for (int i = 0; i < 5; i++) begin
+		address_tb = instructions[i][38:33];
+		writeOn_tb = instructions[i][32];
+		data_in_tb = instructions[i][31:0];
+		#100
+	end
+
+	$stop;
+end
 	
-//2. Internal components
-
-// RAM creation
-  reg [31:0] RAM [0:31]; 
-
-//3. Assignments, Sequential Blocks, and Module Instances:
-  initial begin
-	  $readmemb("Data.txt", RAM);  // Load of data  
-  end
-  
-  always @* begin    
-	if (writeOn) begin  // if flag rises
-	  RAM[address] = data_in;  // Asynchronous writing
-    end
-
-	data_out = RAM[address];  // Asynchronous reading is always on
-  end
-  
 endmodule
