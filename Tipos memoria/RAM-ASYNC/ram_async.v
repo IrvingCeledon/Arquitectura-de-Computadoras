271b15dd075de6fbafb189f5e50a7880f7edb877
IrvingCeledon <irvingdarey@gmail.com>
2025-09-27 22:09:11 -0600

    Update ram_async.v

--- Tipos memoria/RAM-ASYNC/ram_async.v
+++ Tipos memoria/RAM-ASYNC/ram_async.v
@@ -1,1 +1,24 @@
+//1. Module definition
+module ram (address, data_in, data_out, writeOn);
+  input [7:0] address, data_in;
+  output reg [7:0] data_out;
+  input writeOn;
+	
+//2. Internal components
 
+// RAM creation
+  reg [7:0] RAM [0:10]; 
+
+//3. Assignments, Sequential Blocks, and Module Instances:
+  initial
+  
+  always @*
+  begin    
+    if (writeOn) begin
+      RAM[address] = data_in;
+    end
+
+    data_out = RAM[address];
+  end
+  
+endmodule
