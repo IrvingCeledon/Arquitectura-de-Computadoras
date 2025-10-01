1669961231166e2d7b40a4e100ca599b36f889ff
IrvingCeledon <irvingdarey@gmail.com>
2025-09-30 21:15:45 -0600

    Add files via upload

---
+++ Burrito/ram_async.v
@@ -0,0 +1,26 @@
+//1. Module definition
+module ram_async (address, data_in, data_out, writeOn);
+  input [4:0] address;
+  input [31:0] data_in;
+  output reg [31:0] data_out;
+  input writeOn;
+	
+//2. Internal components
+
+// RAM creation
+  reg [31:0] RAM [0:31]; 
+
+//3. Assignments, Sequential Blocks, and Module Instances:
+  initial begin
+	  $readmemb("Data.txt", RAM);  // Load of data  
+  end
+  
+  always @* begin    
+	if (writeOn) begin  // if flag rises
+	  RAM[address] = data_in;  // Asynchronous writing
+    end
+
+	data_out = RAM[address];  // Asynchronous reading is always on
+  end
+  
+endmodule
