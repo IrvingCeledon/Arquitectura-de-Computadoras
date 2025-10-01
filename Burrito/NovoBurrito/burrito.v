9e3a73f6806f48c76060de7efc911a3db3d5401a
IrvingCeledon <irvingdarey@gmail.com>
2025-10-01 12:13:57 -0600

    Update burrito.v

--- Burrito/NovoBurrito/burrito.v
+++ Burrito/NovoBurrito/burrito.v
@@ -1,33 +1,14 @@
 // Dn: Direccion n
 // RD: Register direction
+// AW: Address Write
 module Burrito(Instructions);
 	input [19:0] Instructions;
+	wire [31:0] S1, S2, aluOut;
 
-wire [31:0] S1, S2, aluOut;
+	br BANCO (.WriteOn(Instructions[19]), .RR1(Instructions[14:10]), .RR2(Instructions[9:5]), 
+			      .AW(Instructions[4:0]), .DataIn(aluOut), .RD1(S1), .RD2(S2)
+	         );
 
-br BANCO (.WriteOn(), .RR1(D1), .RR2(D2), .AW(RD),
-	  .DataIn(aluOut), .RD1(S1), .RD2(S2)
-	  );
-
-_ALU alu(.A(S1), .B(S2), .Sel(Op), .C(aluOut)
-	 );
-
-endmodule
-
-// Dn: Direccion n
-// RD: Register direction
-module Burrito(WEnable, Op, D1, D2, RD);
-  input WEnable;  
-  input [3:0] Op;
-  input [4:0] D1, D2, RD;
-
-wire [31:0] S1, S2, aluOut;
-
-br BANCO (.WriteOn(WEnable), .RR1(D1), .RR2(D2), .AW(RD),
-	  .DataIn(aluOut), .RD1(S1), .RD2(S2)
-	  );
-
-_ALU alu(.A(S1), .B(S2), .Sel(Op), .C(aluOut)
-	 );
+	_ALU alu(.A(S1), .B(S2), .Sel(Instructions[18:15]), .C(aluOut));
 
 endmodule
