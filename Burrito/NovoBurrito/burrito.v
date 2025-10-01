190bcb7e67a3c20df818e1b2edd48df912b7ebd4
IrvingCeledon <irvingdarey@gmail.com>
2025-10-01 12:01:40 -0600

    Update burrito.v

--- Burrito/NovoBurrito/burrito.v
+++ Burrito/NovoBurrito/burrito.v
@@ -1,13 +1,11 @@
 // Dn: Direccion n
 // RD: Register direction
-module Burrito(WEnable, Op, D1, D2, RD);
-  input WEnable;  
-  input [3:0] Op;
-  input [4:0] D1, D2, RD;
+module Burrito(Instructions);
+	input [19:0] Instructions;
 
 wire [31:0] S1, S2, aluOut;
 
-br BANCO (.WriteOn(WEnable), .RR1(D1), .RR2(D2), .AW(RD),
+br BANCO (.WriteOn(), .RR1(D1), .RR2(D2), .AW(RD),
 	  .DataIn(aluOut), .RD1(S1), .RD2(S2)
 	  );
 
