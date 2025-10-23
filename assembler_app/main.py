0bce3b0d310e3f840ee5fedd6e1b625f9803536c
IrvingCeledon <irvingdarey@gmail.com>
2025-10-22 22:02:01 -0600

    Update main.py

--- assembler_app/main.py
+++ assembler_app/main.py
@@ -17,6 +17,15 @@ def on_copy():
     window.clipboard_append(bin_output)
     messagebox.showinfo("Copied", "The result has been copied to clipboard.")
     
+def close_program():
+    window.destroy()
+ 
+''' [On Hold]
+def create_std_btn(parent, text_field, method):
+    btn = tk.Button(parent, text_field, method, font=("Arial", 11, "bold"))
+    return btn
+'''
+ 
 # -----------------------
 # Grafic User Interface
 # -----------------------
@@ -25,19 +34,22 @@ tk.Label(window, text="Assembler input:").pack(anchor='w', padx=10)
 text_input = scrolledtext.ScrolledText(window, height=8, width=70, font=("Consolas", 11))
 text_input.pack(padx=10, pady=5, fill='both', expand=True)
 
+input_related_buttons = tk.Frame(window)
+input_related_buttons.pack(pady=5)
+
 tk.Label(window, text="Binary ouput:").pack(anchor='w', padx=10)
 text_output = scrolledtext.ScrolledText(window, height=8, width=70, font=("Consolas", 11), state='disabled')
 text_output.pack(padx=10, pady=5, fill='both', expand=True)
 
 controller = AssemblerController(text_input, text_output)
     
-tk.Button(window, text="Load from file", command=controller.on_load, font=("Arial", 11, "bold")).pack(pady=5)
-tk.Button(window, text="Convert", command=controller.on_convert, font=("Arial", 11, "bold")).pack(pady=5)
-tk.Button(window, text="Clear", command=controller.on_clear, font=("Arial", 11, "bold")).pack(pady=5)
+tk.Button(input_related_buttons, text="Load from file", command=controller.on_load, font=("Arial", 11, "bold")).grid(row=0, column=0, padx=10, pady=5)
+tk.Button(input_related_buttons, text="Convert", command=controller.on_convert, font=("Arial", 11, "bold")).grid(row=0, column=1, padx=10, pady=5)
 
-frame_buttons = tk.Frame(window)
-frame_buttons.pack(pady=5)
-tk.Button(frame_buttons, text="Copy to clipboard", command=on_copy).grid(row=0, column=0, padx=10)
-tk.Button(frame_buttons, text="Save as .txt", command=controller.on_save).grid(row=0, column=1, padx=10)
-
+output_related_buttons = tk.Frame(window)
+output_related_buttons.pack(pady=5)
+tk.Button(output_related_buttons, text="Copy to clipboard", command=on_copy, font=("Arial", 11, "bold")).grid(row=0, column=0, padx=10)
+tk.Button(output_related_buttons, text="Save as .txt", command=controller.on_save, font=("Arial", 11, "bold")).grid(row=0, column=1, padx=10)
+tk.Button(output_related_buttons, text="Clear", command=controller.on_clear, font=("Arial", 11, "bold")).grid(row=0, column=2, padx=10)
+tk.Button(output_related_buttons, text="Exit", command=close_program, font=("Arial", 11, "bold")).grid(row=1, column=0, columnspan=3, padx=10)
 window.mainloop()
