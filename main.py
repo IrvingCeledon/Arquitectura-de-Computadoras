ccc488cca25481cfb25073d3d95725f44cbf89cd
IrvingCeledon <irvingdarey@gmail.com>
2025-10-26 00:40:30 -0600

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from handlers.app_controller import ApplicationController
-
-# This block runs only if the file is executed directly.
-if __name__ == "__main__":
-    app = ApplicationController()
-    app.run()
