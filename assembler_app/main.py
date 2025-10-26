ee78b5f7d494e2ba184c799ffccf3b309d2b4c3e
IrvingCeledon <irvingdarey@gmail.com>
2025-10-26 00:38:25 -0600

    Create main.py

---
+++ assembler_app/main.py
@@ -0,0 +1,6 @@
+from handlers.app_controller import ApplicationController
+
+# This block runs only if the file is executed directly.
+if __name__ == "__main__":
+    app = ApplicationController()
+    app.run()
