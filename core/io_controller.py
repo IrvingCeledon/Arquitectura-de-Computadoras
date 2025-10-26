fb84ccaf41823b6ff8e29aeb499a8bc3227e9046
IrvingCeledon <irvingdarey@gmail.com>
2025-10-26 00:37:10 -0600

    Add files via upload

---
+++ core/io_controller.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import messagebox
+from . import io_utils
+
+class IOController:
+    def __init__(self, input_widget, output_widget):
+        self.input_widget = input_widget
+        self.output_widget = output_widget
+
+    def on_load(self):
+        file_content = io_utils.load_from_file()
+        
+        if file_content:
+            self.input_widget.delete("1.0", tk.END)
+            self.input_widget.insert(tk.END, file_content)
+            
+    def on_save(self):
+        text = self.output_widget.get("1.0", tk.END).strip()
+        
+        try:
+            io_utils.save_as_txt(text)
+        except ValueError as e:
+            messagebox.showerror("Error", str(e))
+            
+    def set_text_containers(self, input_widget, output_widget): 
+        self.input_widget = input_widget
+        self.output_widget = output_widget
