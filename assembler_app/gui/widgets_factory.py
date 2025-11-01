ad928fa467cc9f3bb590b4c48744de45da0a6847
IrvingCeledon <irvingdarey@gmail.com>
2025-10-31 22:46:00 -0600

    Update widgets_factory.py

--- assembler_app/gui/widgets_factory.py
+++ assembler_app/gui/widgets_factory.py
@@ -1,14 +1,13 @@
-import tkinter as tk
+from tkinter import ttk
+ 
+def create_button(parent, text_field, function, style_name="Generic.TButton"):
+    return ttk.Button(parent, text=text_field, command=function, style=style_name)
 
-# I have to make more generic fuctions for ttk widgets or update these ones.
-def create_button(parent, text_field, fuction):
-    return tk.Button(parent, text=text_field, command=fuction, font=("Arial", 11, "bold"))
-    
 def create_frames(parent, pady_value, fill_value, range_size):
-    generic_frame = tk.Frame(parent)
+    generic_frame = ttk.Frame(parent)
     generic_frame.pack(pady=pady_value, fill=fill_value)
-        
+    
     for i in range(range_size):
         generic_frame.grid_columnconfigure(i, weight=1, uniform="btn")
-       
+        
     return generic_frame
