cf138ae4b139670cf4c33cf0a0b776aafaf26137
IrvingCeledon <irvingdarey@gmail.com>
2025-11-18 22:11:13 -0600

    Add files via upload

---
+++ assembler_app/gui/resources/translations.py
@@ -0,0 +1,26 @@
+translations = {
+    "English": {
+        "input_label": "Assembler input:",
+        "load_button": "Load from file",
+        "convert_button": "Convert",
+        "output_label": "Binary output:",
+        "copy_to_clipboard_button": "Copy to clipboard",
+        "save_as_txt_button": "Save as .txt",
+        "clear_button": "Clear",
+        "exit_button": "Exit",
+        "settings_button": "Settings"
+    },
+
+    "Spanish": {
+        "input_label": "Entrada en ensamblador:",
+        "load_button": "Cargar desde un archivo",
+        "convert_button": "Procesar",
+        "output_label": "Salida en binario:",
+        "copy_to_clipboard_button": "Copiar al portapapeles",
+        "save_as_txt_button": "Guardar como archivo .txt",
+        "clear_button": "Limpiar",
+        "exit_button": "Salir",
+        "settings_button": "Ajustes"
+        
+    }
+}
