7ee46e0693ff78b84f62d1e110f79563cacfc3eb
IrvingCeledon <irvingdarey@gmail.com>
2025-11-20 16:26:41 -0600

    Update app_controller.py

--- assembler_app/handlers/app_controller.py
+++ assembler_app/handlers/app_controller.py
@@ -4,6 +4,7 @@ from gui.main_window import MainWindow
 from core.assembler_controller import AssemblerController
 from core.io_controller import IOController
 from core.settings_manager import SettingsManager
+from resources.translations import translations
 
 class ApplicationController:
     def __init__(self):
@@ -15,7 +16,19 @@ class ApplicationController:
         self.assembler_controller = AssemblerController(None, None, self.settings)
         self.io_controller = IOController(None, None)
         init_styles()
-        self.main_window = MainWindow(self.root, self.assembler_controller, self.io_controller, self.settings)
+        self.main_window = MainWindow(self.root, self.assembler_controller, self.io_controller, self.settings, self.set_language)
+        
+        self.set_language()
 
     def run(self):
         self.root.mainloop()
+
+    # Consider making a get_language function.
+    def set_language(self):
+        language = self.settings.get("language")
+        
+        if language == "English": return
+        
+        tr = translations[language]
+        self.root.title(tr["app_title"])
+        self.main_window.apply_language(tr)
