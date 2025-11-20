9269696f5d8b3ed64601045d27d69388f7303cb9
IrvingCeledon <irvingdarey@gmail.com>
2025-11-20 16:26:59 -0600

    Update translations.py

--- assembler_app/resources/translations.py
+++ assembler_app/resources/translations.py
@@ -1,26 +1,35 @@
 translations = {
-    "English": {
-        "input_label": "Assembler input:",
-        "load_button": "Load from file",
-        "convert_button": "Convert",
-        "output_label": "Binary output:",
-        "copy_to_clipboard_button": "Copy to clipboard",
-        "save_as_txt_button": "Save as .txt",
-        "clear_button": "Clear",
-        "exit_button": "Exit",
-        "settings_button": "Settings"
-    },
-
     "Spanish": {
-        "input_label": "Entrada en ensamblador:",
-        "load_button": "Cargar desde un archivo",
-        "convert_button": "Procesar",
-        "output_label": "Salida en binario:",
-        "copy_to_clipboard_button": "Copiar al portapapeles",
-        "save_as_txt_button": "Guardar como archivo .txt",
-        "clear_button": "Limpiar",
-        "exit_button": "Salir",
-        "settings_button": "Ajustes"
+        # ----- AppTitle -----
+        "app_title": "Ensamblador a Binario",
         
+        # ----- MainWindow -----
+        "input_lbl": "Entrada en ensamblador:",
+        "load_btn": "Cargar desde un archivo",
+        "convert_btn": "Procesar",
+        "output_lbl": "Salida en binario:",
+        "copy_to_clipboard_btn": "Copiar al portapapeles",
+        "save_as_txt_btn": "Guardar como archivo .txt",
+        "clear_btn": "Limpiar",
+        "exit_btn": "Salir",
+        "settings_btn": "Ajustes",
+        
+        # ----- SettingsWindow -----
+        "window_title": "Ajustes",
+        "settings_title": "Ajustes generales",
+        "language_lbl": "Idioma",
+        "en_EN": "Ingles",
+        "es_ES": "Español",
+        "format_options_lbl": "Opciones de formato",
+        "dark_mode_checkbox": "Cambiar a modo oscuro",
+        "bit_format_checkbox": "Usar formato de salida de 32'b",
+        "clean_options_lbl": "Opciones de limpiar",
+        "clean_input_checkbox": "Limpiar entrada",
+        "clean_output_checkbox": "Limpiar salida",
+        "decode_options_lbl": "Opciones de decodificacion",
+        "accumulate_checkbox": "Acumalar resultados binarios en la salida",
+        "truncate_checkbox": "Truncar valores enteros de las entradas",
+        "apply_btn": "Aplicar",
+        "close_btn": "Cerrar"
     }
 }
