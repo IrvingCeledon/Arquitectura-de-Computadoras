67093f94adf1c1dfe4cd7e22fa5ba3b4a1e5987d
IrvingCeledon <irvingdarey@gmail.com>
2025-11-21 15:50:10 -0600

    Update translations.py

--- assembler_app/resources/translations.py
+++ assembler_app/resources/translations.py
@@ -1,9 +1,9 @@
 translations = {
     "en_US": {
-        # ----- AppTitle -----
+        # ----- AppTitle ----- #
         "app_title": "Assembler to Binary",
         
-        # ----- MainWindow -----
+        # ----- MainWindow ----- #
         "input_lbl": "Assembler input:",
         "load_btn": "Load from file",
         "convert_btn": "Convert",
@@ -14,7 +14,19 @@ translations = {
         "exit_btn": "Exit",
         "settings_btn": "Settings",
         
-        # ----- SettingsWindow -----
+        # Info messages:
+        "empty_output_title": "Empty fields",
+        "empty_output_message": "There is nothing to copy",
+        "copied_title": "Copied!",
+        "copied_message": "The result has been copied to clipboard",
+        "nothing_to_clear_title": "Nothing to clear",
+        "nothing_to_clear_message": "input and output are already empty",
+        "cannot_clean_input_title": "Input empty",
+        "cannot_clean_input_message": "Cannot clean, input is already empty",        
+        "cannot_clean_output_title": "Output empty",
+        "cannot_clean_output_message": "Cannot clean, output is already empty",     
+        
+        # ----- SettingsWindow ----- #
         "window_title": "Settings",
         "settings_title": "Settings",
         "language_lbl": "Language",
@@ -29,14 +41,34 @@ translations = {
         "decode_options_lbl": "Decode options",
         "accumulate_checkbox": "Accumulate results in binary output",
         "truncate_checkbox": "Truncate integer inputs",
-        "close_btn": "Close"
-    },
+        "close_btn": "Close",
+        
+        # Error messages:
+        "invalid_config_title": "Invalid configuration",
+        "invalid_config_message": "At least one of 'Clean input' or 'Clean output' must be selected",
+        
+        # Info messages:
+        "info_dark_mode_title": "Working on this, wait for it!",
+        "info_dark_mode_message": "Dark mode not available",
+        "perm_required_title": "Permission required",
+        "perm_required_message": "Developer privileges are required",
+        
+        # ----- IOController ----- #
+        "missing_fields_info_title": "Missing fields",
+        
+        # ----- IOUtils ----- #
+        "select_file_title": "Select a file",
+        "txt_option": "Text file",
+        "bin_option": "Binary",
+        "all_option": "All files",
+        
+    },# self.tr[""]
     
     "es_MX": {
-        # ----- AppTitle -----
+        # ----- AppTitle ----- #
         "app_title": "Ensamblador a Binario",
         
-        # ----- MainWindow -----
+        # ----- MainWindow ----- #
         "input_lbl": "Entrada en ensamblador:",
         "load_btn": "Cargar desde un archivo",
         "convert_btn": "Procesar",
@@ -46,8 +78,20 @@ translations = {
         "clear_btn": "Limpiar",
         "exit_btn": "Salir",
         "settings_btn": "Ajustes",
+
+        # Info messages:
+        "empty_output_title": "Campos vacíos",
+        "empty_output_message": "No hay nada para copiar",
+        "copied_title": "Copiado!",
+        "copied_message": "El resultado se ha copiado al portapapeles",
+        "nothing_to_clear_title": "Nada por limpiar",
+        "nothing_to_clear_message": "La entrada y salida ya están vacías",
+        "cannot_clean_input_title": "Entrada vacía",
+        "cannot_clean_input_message": "La entrada ya se encuentra limpia",        
+        "cannot_clean_output_title": "Salida vacía",
+        "cannot_clean_output_message": "La salida ya se encuentra limpia",            
         
-        # ----- SettingsWindow -----
+        # ----- SettingsWindow ----- #
         "window_title": "Ajustes",
         "settings_title": "Ajustes generales",
         "language_lbl": "Idioma",
@@ -62,6 +106,25 @@ translations = {
         "decode_options_lbl": "Opciones de decodificación",
         "accumulate_checkbox": "Acumalar resultados binarios en la salida",
         "truncate_checkbox": "Truncar enteros de las entradas",
-        "close_btn": "Cerrar"
+        "close_btn": "Cerrar",
+        
+        # Error messages:
+        "invalid_config_title" : "Configuración inválida",
+        "invalid_config_message" : "Debe haber al menos una opción de limpieza seleccionada",
+        
+        # Info messages:
+        "info_dark_mode_title" : "¡Función en construcción!",
+        "info_dark_mode_message" : "El modo oscuro no esta disponible por el momento",
+        "perm_required_title" : "Permiso denegado",
+        "perm_required_message" : "Se necesitan privilegios de desarrollador para continuar",
+    
+        # ----- IOController ----- #
+        "missing_fields_info_title": "Campos vacíos",
+        
+        # ----- IOUtils ----- #
+        "select_file_title": "Selecciona un archivo",
+        "txt_option": "Archivo de texto",
+        "bin_option": "Binario",
+        "all_option": "Todos los archivos",
     }
 }
