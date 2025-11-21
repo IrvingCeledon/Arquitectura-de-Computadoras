26abf53bc5584907f4e98c365292369288d41beb
IrvingCeledon <irvingdarey@gmail.com>
2025-11-20 21:41:54 -0600

    Update app_controller.py

--- assembler_app/handlers/app_controller.py
+++ assembler_app/handlers/app_controller.py
@@ -4,31 +4,35 @@ from gui.main_window import MainWindow
 from core.assembler_controller import AssemblerController
 from core.io_controller import IOController
 from core.settings_manager import SettingsManager
-from resources.translations import translations
 
 class ApplicationController:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("Assembler to Binary")
         self.root.geometry("700x580")
         
+        from resources.translations import translations
+        
         self.settings = SettingsManager()
+        self.current_language = self.settings.get("language")
+        self.language_dictionary = translations[self.current_language]
+        self.root.title(self.language_dictionary["app_title"])
+        
         self.assembler_controller = AssemblerController(None, None, self.settings)
         self.io_controller = IOController(None, None)
         init_styles()
-        self.main_window = MainWindow(self.root, self.assembler_controller, self.io_controller, self.settings, self.set_language)
-        
-        self.set_language()
+        self.main_window = MainWindow(self.root, self.assembler_controller, self.io_controller, self.settings, self.language_dictionary, self.set_language)
 
     def run(self):
         self.root.mainloop()
 
     # Consider making a get_language function.
     def set_language(self):
-        language = self.settings.get("language")
+        from resources.translations import translations
         
-        if language == "English": return
+        if self.current_language == self.settings.get("language"): return
         
-        tr = translations[language]
-        self.root.title(tr["app_title"])
-        self.main_window.apply_language(tr)
+        self.current_language = self.settings.get("language")
+        self.language_dictionary = translations[self.current_language]
+        
+        self.root.title(self.language_dictionary["app_title"])
+        self.main_window.apply_language(self.language_dictionary)
