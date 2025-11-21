529d0f8a98180e6bae082d1cc20c096c5ee95682
IrvingCeledon <irvingdarey@gmail.com>
2025-11-21 15:48:04 -0600

    Update io_controller.py

--- assembler_app/core/io_controller.py
+++ assembler_app/core/io_controller.py
@@ -3,12 +3,15 @@ from tkinter import messagebox
 from . import io_utils
 
 class IOController:
-    def __init__(self, input_widget, output_widget):
+    def __init__(self, input_widget, output_widget, language_dictionary):
         self.input_widget = input_widget
         self.output_widget = output_widget
+        self.tr = language_dictionary
+        
+        self.utils = io_utils.IOUtils(self.tr)
 
     def on_load(self):
-        file_content = io_utils.load_from_file()
+        file_content = self.utils.load_from_file()
         
         if file_content:
             self.input_widget.delete("1.0", tk.END)
@@ -18,10 +21,14 @@ class IOController:
         text = self.output_widget.get("1.0", tk.END).strip()
         
         try:
-            io_utils.save_as_txt(text)
+            self.utils.save_as_txt(text)
         except ValueError as e:
-            messagebox.showinfo("Missing fields", str(e))
+            messagebox.showinfo(self.tr["missing_fields_info_title"], str(e))
             
     def set_text_containers(self, input_widget, output_widget): 
         self.input_widget = input_widget
         self.output_widget = output_widget
+        
+    def apply_language(self, new_tr):
+        self.tr = new_tr
+        self.utils.apply_language(self.tr)
