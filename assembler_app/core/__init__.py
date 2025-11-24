6f13365ddc9cef3e03d6123907f93414b1afb68d
IrvingCeledon <irvingdarey@gmail.com>
2025-11-23 19:18:19 -0600

    Add files via upload

--- assembler_app/core/__init__.py
+++ assembler_app/core/__init__.py
@@ -1,1 +1,5 @@
+from .assembler_controller import AssemblerController
+from .io_controller import IOController
+from .settings_manager import SettingsManager
 
+__all__ = ["AssemblerController", "IOController", "SettingsManager"]
--- assembler_app/core/assembler.py
+++ assembler_app/core/assembler.py
@@ -1,9 +1,10 @@
-from resources import assembler_data
+from resources import INSTRUCTIONS
 
 class Assembler:
     def __init__(self, language_dictionary):
         self.tr = language_dictionary 
 
+    # ----- Public methods ----- # 
     # This method works as an error handler for decoding. It receives user's input and returns the output.
     def assembler_to_binary(self, text_input):
         # Checks if the user input is empty and removes possible initial Tkinter values. 
@@ -16,106 +17,107 @@ class Assembler:
         instructions = assembly_input.split('\n')
         
         try: # Delegates the decoding process to format_instructions.
-            return self.format_instructions(instructions)
+            return self._format_instructions(instructions)
         except ValueError as e:
             raise ValueError(f"{e}")
 
+    def apply_language(self, new_tr):
+        self.tr = new_tr
+
+    # ----- Internal methods ----- # 
     # Formats one or multiple assembly instructions by parsing each and converting them to binary.
-    def format_instructions(self, instructions):
-        # Auxiliary list to store the resulting binary instructions, whether a single instruction or several.
+    def _format_instructions(self, instructions):
+        # Auxiliary array to store the resulting binary instructions, whether a single instruction or several.
         formatted_instructions = []
         
         for instr in instructions:
             # Removes possible residual '\n' characters or extra spaces.
             if instr.strip():
-                parsed_instruction = self.parser(instr)
-                binary_instruction = self.format_binary(parsed_instruction)
+                parsed_instruction = self._parser(instr)
+                binary_instruction = self._format_binary(parsed_instruction)
                 formatted_instructions.append(binary_instruction)
-            
+
         # Returns all binary instructions separated by a newline.   
         return "\n".join(formatted_instructions)
-            
-    def parser(self, assembly_input : str):
+ 
+    def _parser(self, assembly_input : str):
         fields = assembly_input.split('$')
         instr_op = fields[0].upper().strip()
         
-        instr_info = assembler_data.INSTRUCTIONS.get(instr_op)
+        instr_info = INSTRUCTIONS.get(instr_op)
         if instr_info is None :
-            raise ValueError(f"{self.tr['invalid_isntruction_flag']} {instr_op}")
-            
+            raise ValueError(f"{self.tr['invalid_instruction_flag']} {instr_op}")
+ 
         instr_type = instr_info["type"]
         
         if instr_type == "R":
-            return self.parser_r_fields(instr_info, fields)
+            return self._parser_r_fields(instr_info, fields)
         elif instr_type == "I":
-            return self.parser_i_fields(instr_info, fields)
+            return self._parser_i_fields(instr_info, fields)
 
-    def parser_r_fields(self, instr_info, fields):
-        self.validate_range(fields, 4)
+    def _parser_r_fields(self, instr_info, fields):
+        self._validate_range(fields, 4)
         numbers = [fields[1], fields[2], fields[3]]
-            
-        return self.instruction_builder(instr_info, self.to_int(numbers))
-        
-    def parser_i_fields(self, instr_info, fields):
-        self.validate_range(fields, 3)
-            
+
+        return self._instruction_builder(instr_info, self._to_int(numbers))
+  
+    def _parser_i_fields(self, instr_info, fields):
+        self._validate_range(fields, 3)
+   
         if '#' not in fields[2]:
             raise ValueError(f"{self.tr['hash_not_found_flag']} {fields[2]}")
-            
+  
         parts = fields[2].split('#')
         numbers = [fields[1], parts[0], parts[1]]    
-            
-        return self.instruction_builder(instr_info, self.to_int(numbers))
-        
-    def validate_range(self, data, limit_bound):
+  
+        return self._instruction_builder(instr_info, self._to_int(numbers))
+  
+    def _validate_range(self, data, limit_bound):
         if len(data) > limit_bound :
             raise ValueError(self.tr["upper_limit_flag"])
         elif len(data) < limit_bound :
             raise ValueError(self.tr["lower_limit_flag"])
 
-    def to_int(self, data):
+    def _to_int(self, data):
         try:
             print("DEBUG → to_int:", data) 
             return [int(p.strip()) for p in data]
         except ValueError:
             raise ValueError(self.tr["is_not_integer_flag"])
-        
-    def instruction_builder(self, type_info, numbers_data):
+
+    def _instruction_builder(self, type_info, numbers_data):
         instr_type = type_info["type"]
         print("DEBUG → instruction_builder:", numbers_data) 
         
         if instr_type == "R":
-            return self.build_r_type(type_info, numbers_data)
+            return self._build_r_type(type_info, numbers_data)
         elif instr_type == "I":
-            return self.build_i_type(type_info, numbers_data)
+            return self._build_i_type(type_info, numbers_data)
 
-    def build_r_type(self, info, data):
+    def _build_r_type(self, info, data):
         """Builds the binary instruction (R-type MIPS)."""
         op  = f"{int(info['opcode']):06b}"
-        rs  = f"{self.truncate_to_bits(data[1], 5):05b}"
-        rt  = f"{self.truncate_to_bits(data[2], 5):05b}"
-        rd  = f"{self.truncate_to_bits(data[0], 5):05b}"
+        rs  = f"{self._truncate_to_bits(data[1], 5):05b}"
+        rt  = f"{self._truncate_to_bits(data[2], 5):05b}"
+        rd  = f"{self._truncate_to_bits(data[0], 5):05b}"
         sh  = f"{int(info['shamt']):05b}"
         fnc = f"{int(info['funct']):06b}"
 
         return (op + rs + rt + rd + sh + fnc)
 
-    def build_i_type(self, info, data):
+    def _build_i_type(self, info, data):
         """Builds the binary instruction (I-type MIPS)."""
         op  = f"{int(info['opcode']):06b}"
-        rs  = f"{self.truncate_to_bits(data[1], 5):05b}"
-        rt  = f"{self.truncate_to_bits(data[0], 5):05b}"
-        inm = f"{self.truncate_to_bits(data[2], 16):016b}"
+        rs  = f"{self._truncate_to_bits(data[1], 5):05b}"
+        rt  = f"{self._truncate_to_bits(data[0], 5):05b}"
+        inm = f"{self._truncate_to_bits(data[2], 16):016b}"
         print("DEBUG → build_i_type:", data) 
         
         return (op + rs + rt + inm)
-        
-    def truncate_to_bits(self, value, bits):
+ 
+    def _truncate_to_bits(self, value, bits):
         mask = (1 << bits) - 1 
         return value & mask
-        
-    def format_binary(self, binary : str):
-        return " ".join([binary[i:i+8] for i in range(0, len(binary), 8)])
-        
-    def apply_language(self, new_tr):
-        self.tr = new_tr
+  
+    def _format_binary(self, binary : str):
+        return " ".join([binary[i:i+8] for i in range(0, len(binary), 8)]) 
