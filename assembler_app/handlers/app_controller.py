e05c5c349485c12517777fc31bc0d68f20a840a1
IrvingCeledon <irvingdarey@gmail.com>
2025-11-23 19:20:51 -0600

    Add files via upload

--- assembler_app/handlers/app_controller.py
+++ assembler_app/handlers/app_controller.py
@@ -1,41 +1,65 @@
 import tkinter as tk
-from gui.styles import init_styles
-from gui.main_window import MainWindow
-from core.assembler_controller import AssemblerController
-from core.io_controller import IOController
-from core.settings_manager import SettingsManager
+from resources import TRANSLATIONS
+from core import AssemblerController, IOController, SettingsManager
+from gui import init_styles, MainWindow
 
 class ApplicationController:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("700x580")
-        
-        from resources.translations import translations
-        
         self.settings = SettingsManager()
+        
+        self.current_language = None
+        self.language_dictionary = None
+        
+        self._configure_window()
+        self._load_language()
+        self._init_controllers()
+        self._init_gui()
+
+    # ----- Internal Methods ----- #
+    def _configure_window(self):
+        self.root.geometry("700x580")
+
+    def _load_language(self):
         self.current_language = self.settings.get("language")
-        self.language_dictionary = translations[self.current_language]
-        self.root.title(self.language_dictionary["app_title"])
+        self.language_dictionary = TRANSLATIONS[self.current_language]
+        self.root.title(self.language_dictionary["app_title"])    
+   
+    def _init_controllers(self):
+        self.assembler_controller = AssemblerController(
+            None, None, self.settings, self.language_dictionary
+        )
         
-        self.assembler_controller = AssemblerController(None, None, self.settings, self.language_dictionary)
-        self.io_controller = IOController(None, None, self.language_dictionary)
+        self.io_controller = IOController(
+            None, None, self.language_dictionary
+        )
+   
+    def _init_gui(self):
         init_styles()
-        self.main_window = MainWindow(self.root, self.assembler_controller, self.io_controller, self.settings, self.language_dictionary, self.set_language)
-
-    def run(self):
-        self.root.mainloop()
-
-    # Consider making a get_language function.
-    def set_language(self):
-        from resources.translations import translations
         
-        if self.current_language == self.settings.get("language"): 
+        self.main_window = MainWindow(
+            self.root, 
+            self.assembler_controller, 
+            self.io_controller, 
+            self.settings, 
+            self.language_dictionary, 
+            self.refresh_language
+        )
+
+    # ----- Public Methods ----- #
+    def refresh_language(self):
+        new_language = self.settings.get("language")
+        
+        if self.current_language == new_language: 
             return
         
-        self.current_language = self.settings.get("language")
-        self.language_dictionary = translations[self.current_language]
+        self.current_language = new_language
+        self.language_dictionary = TRANSLATIONS[self.current_language]
         
         self.root.title(self.language_dictionary["app_title"])
         self.assembler_controller.apply_language(self.language_dictionary)
         self.io_controller.apply_language(self.language_dictionary)
         self.main_window.apply_language(self.language_dictionary)
+  
+    def run(self):
+        self.root.mainloop()
