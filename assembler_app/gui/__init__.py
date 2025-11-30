76403c64f654440cc214f6a7575ba7da8da54241
IrvingCeledon <irvingdarey@gmail.com>
2025-11-30 02:00:44 -0600

    Add files via upload

--- assembler_app/gui/__init__.py
+++ assembler_app/gui/__init__.py
@@ -1,12 +1,12 @@
 from .styles import init_styles
 from .widgets_factory import create_button, create_frames
-from .settings_window import SettingsWindow
-from .main_window import MainWindow
+from .sw import SettingsWindow
+from .mw import MainWindow
 
 __all__ = [
     "init_styles",
     "create_button",
     "create_frames",
-    "SettingsWindow",
+    "SettingsWindow"
     "MainWindow"
 ]
