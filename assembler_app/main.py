e43a3234bff368ee900a63e0b11bec48b43e636c
IrvingCeledon <irvingdarey@gmail.com>
2025-11-30 01:57:20 -0600

    Update main.py

--- assembler_app/main.py
+++ assembler_app/main.py
@@ -3,9 +3,13 @@ from handlers import ApplicationController
 
 def main():
     tracemalloc.start()
+    
     app = ApplicationController()
     app.run()
-    print(tracemalloc.get_traced_memory())
+    
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"Memoria actual: {current / 1024:.2f} KB")
+    print(f"Memoria pico: {peak / 1024:.2f} KB")
     
 if __name__ == "__main__":
     main()
